314fbf135267a7c497cc6b4b9f2db827a8d2623c
Aditya <adityasharma2812@gmail.com>
2025-02-11 13:18:26 +0530

    Creating the candidates api

--- ats/views.py
+++ ats/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Candidate
+from .serializers import CandidateSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Q
 
-# Create your views here.
+class CandidateViewSet(viewsets.ModelViewSet):
+    queryset = Candidate.objects.all()
+    serializer_class = CandidateSerializer
+
+    @action(detail=False, methods=['get'], url_path='search')
+    def search(self, request):
+        keyword = request.query_params.get('query', None)
+        q = Q()
+        if keyword and type(keyword) == str:
+            for k in keyword.split(" "):
+                q |= Q(name__icontains=k)
+            candidates = Candidate.objects.filter(q)
+        else:
+            candidates = Candidate.objects.all()
+        serializer = CandidateSerializer(candidates, many=True)
+        return Response(serializer.data)
